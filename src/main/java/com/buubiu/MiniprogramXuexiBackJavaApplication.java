b0dc294f46eaddc33f3aa0c44de5622268438626
bufx <bufanxiang@gmail.com>
2021-12-01 14:05:37 +0800

    初始化

---
+++ src/main/java/com/buubiu/MiniprogramXuexiBackJavaApplication.java
@@ -0,0 +1,13 @@
+package com.buubiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprogramXuexiBackJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprogramXuexiBackJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buubiu/MiniprogramXuexiBackJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buubiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprogramXuexiBackJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
